967ef292d835606a143128effa970bf2ccac3928
nyarw <github@xwalfie.dev>
2026-08-08 21:19:02 +0200

    feat: add discord server embed and move useHash into a hook

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,16 @@
 "use client";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+
+import { useHash } from "@/hooks/useHash";
 
 export default function Nav() {
 	const topbar_thing_button =
 		"w-1/8 min-w-max h-max hover:bg-ctp-base hover:border-b-ctp-mauve border-b-4 transition-border duration-100"; // let me tell you how much I HATE tailwind
 	const a = "size-full py-5 block";
 
-	var [state, set] = useState("#about");
+	const hash = useHash();
 	const thing = (name: string) =>
-		`${topbar_thing_button} ${state === name ? "border-b-ctp-mauve" : "border-b-transparent"}`;
-	useEffect(() => {
-		addEventListener("hashchange", () => {
-			set(window.location.hash);
-		});
-	});
+		`${topbar_thing_button} ${hash === name ? "border-b-ctp-mauve" : "border-b-transparent"}`;
+
 	return (
 		<nav className="text-xl big-font bg-ctp-mantle">
 			<ul className="flex w-full justify-center text-center gap-5 text-4xl">
@@ -33,10 +29,10 @@ export default function Nav() {
 						founders
 					</a>
 				</li>
-				<li className={thing("discord")}>
-					<Link href="/discord" className={a}>
+				<li className={thing("#discord")}>
+					<a href="#discord" className={a}>
 						discord
-					</Link>
+					</a>
 				</li>
 			</ul>
 		</nav>
