294904f724dfa26424d7f7b14faf88f8ee43c623
nyarw <github@xwalfie.dev>
2026-08-08 18:43:54 +0200

    feat: add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
-export default function Home() {
-	return;
+export default function About() {
+	return (
+		<section id="about" className="flex flex-col items-center justify-center">
+			<Image src="/cail.png" alt="logo" width={256} height={256} />
+			<h1 className="text-4xl">Lawn Meowers Council</h1>
+			<p className="text-2xl">meowing commeownity :3</p>
+		</section>
+	);
 }
