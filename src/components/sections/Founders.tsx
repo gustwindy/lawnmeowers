e49da5d572e6a6e4758b9e1f25b4442fdd6abc20
Gust Windy <76622722+gustwindy@users.noreply.github.com>
2026-08-10 02:02:54 +0800

    iiimmm sooooooo gaaayyyyyyyy and lesbian

--- src/components/sections/Founders.tsx
+++ src/components/sections/Founders.tsx
@@ -1,9 +1,24 @@
 import Section from "@/components/Section";
+import profilesData from "@/data/profiles.json";
+import type { Profile } from "@/types/profile";
+
+import ProfileCard from "../ProfileCard";
 
 export default function Founders({ active }: { active: boolean }) {
 	return (
 		<Section id="founders" active={active}>
-			<h2 className="text-3xl">Founders</h2>
+			<h2 className="text-3xl m-5 mb-auto">Founders</h2>
+			<ul className="m-5 flex flex-wrap justify-center max-w-3/4 min-w-2xl">
+				{(profilesData.profiles as Profile[])
+					.filter((w) => w.roles.includes("Founder"))
+					.map((profile) => (
+						<ProfileCard
+							key={profile.username}
+							profile={profile}
+							expand={false}
+						/>
+					))}
+			</ul>
 		</Section>
 	);
 }
