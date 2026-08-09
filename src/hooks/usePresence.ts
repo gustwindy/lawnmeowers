9d3c527b3a2c6992e05f032a4bc2c6fe7be8ceaf
nyarw <github@xwalfie.dev>
2026-08-09 20:27:45 +0200

    FEA:t ACTIIVETES IT WORKS YES

--- src/hooks/usePresence.ts
+++ src/hooks/usePresence.ts
@@ -2,6 +2,9 @@ import { useLanyard } from "use-lanyard";
 
 import type { Profile } from "@/types/profile";
 
+type Presence = NonNullable<ReturnType<typeof usePresence>>;
+export type Activity = Presence["activities"][number];
+
 export function usePresence(discordId: Profile["discordId"]) {
 	return useLanyard(discordId ?? "0", {
 		api: {
@@ -9,4 +12,20 @@ export function usePresence(discordId: Profile["discordId"]) {
 			secure: true,
 		},
 	});
+}
+
+export function getActivityImageUrl(activity: Activity) {
+	const image = activity.assets?.large_image;
+
+	if (!image) return null;
+
+	if (image.startsWith("spotify:")) {
+		return `https://i.scdn.co/image/${image.slice(8)}`;
+	}
+
+	if (image.startsWith("mp:external/")) {
+		return `https://media.discordapp.net/${image.slice(3)}`;
+	}
+
+	return `https://cdn.discordapp.com/app-assets/${activity.application_id}/${image}.png`;
 }
