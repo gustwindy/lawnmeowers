fc4eefe18aca063943cc43fc51a5d00dcc26b2d8
nyarw <github@xwalfie.dev>
2026-08-11 15:02:31 +0200

    feat: added oneko

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Script from "next/script";
+
 import Nav from "@/components/Nav";
 
 const geistSans = Geist({
@@ -46,6 +48,12 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
 			<body className="min-h-full flex flex-col bg-ctp-base dark:bg-ctp-crust text-ctp-text">
 				<Nav />
 				{children}
+
+				<Script
+					src="/oneko.js"
+					strategy="afterInteractive"
+					data-cat="/oneko.gif"
+				/>
 			</body>
 		</html>
 	);
